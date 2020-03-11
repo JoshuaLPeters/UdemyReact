b327d8d3045309258a912f7cf46a603695ea918a
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-11 17:57:34 -0500

    saving project progress

---
+++ firstReactDirectory/firstReactJS.js
@@ -0,0 +1,11 @@
+function Person() {
+    return(
+        <div class="person">
+        <h1>Joshua</h1>
+        <p>Your age: 32</p>
+    </div>
+    );
+}
+
+ReactDOM.render(<Person/>,
+    document.querySelector(#p1));
